cf01ac6f755c632a5d24be8140fe362a375658bd
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-13 23:16:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,8 +72,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.each do |season, holiday|
-    holiday.map do |holidays, value|
-      holidays if value.include?("BBQ")
+    holiday.map do |holiday, value|
+      holiday if value.include?("BBQ")
     end
   end.flatten.compact
 end
