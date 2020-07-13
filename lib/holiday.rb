3bfa534dd1a5bfea2ce386c22708958c656dec58
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-13 22:52:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,9 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
     puts "#{seaon.capitalize}:"
-    holiday.each do |
+    holiday.each do |holiday, item|
+      
+    end
   end
 end
 
